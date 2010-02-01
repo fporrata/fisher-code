ed0155985a1bf9e6df4f1587a9a9fc40dfb035b3
yufu.fisher <yufu.fisher@949dbb96-0d51-11df-8f49-cf7c468afe83>
2010-02-01 04:22:53 +0000

    ZernQuadNum experiment is finished.

--- na/ChebyZernLSRBFComp/ZernQuadNum/ZernQuadNum.m
+++ na/ChebyZernLSRBFComp/ZernQuadNum/ZernQuadNum.m
@@ -13,7 +13,7 @@ s4 = 'cos(10*r''*cos(theta))';
 s5 = 'ChebQuad(r'', theta, 0, 0)';
 
 s6 = 'cos(5 * r'' * cos(theta))';
-s7 = 'exp(-3*(r''*cos(theta)).^2+(r''*sin(theta)).^2)./(1+r''.^2)';
+s7 = 'exp(-3*(r''*cos(theta)).^2+(r''*sin(theta)).^2)./((1+r''.^2) * ones(1, length(theta)))';
 
 s8 = '1 ./ (1 + r'' .^ 2)';
 s9 = '1 ./ (1 + 3 ^ 2 * r'' .^ 2) * ones(1, length(theta))';
@@ -33,7 +33,7 @@ s18 = '(6 * r''.^4  - 6*r''.^2 +1) * ones(1, length(theta))'; % R_4^0
 s19 = 'r''.^10 .* (-3003 + 21840 * r''.^2 - 61880 * r''.^4 + 85680 * r''.^6 - 58140 * r''.^8 + 15504 * r''.^10) * cos(10 * theta)';
 s20 = 'r''.^20 .* (30045015 - 443521650 * r''.^2 + 2903050800 * r''.^4 - 11107324800 * r''.^6 + 27536909400 * r''.^8 - 46262007792 * r''.^10 + 53379239760 * r''.^12 - 41799616320 * r''.^14 + 21273019020 * r''.^16 - 6357453960 * r''.^18 + 847660528 * r''.^20) * cos(20 * theta)';
 s21 = 'r''.^9 .* (-2002 + 15015 * r''.^2 - 43680 * r''.^4 + 61880 * r''.^6 - 42840 * r''.^8 + 11628 * r''.^10) * cos(9 * theta)';
-func_str = s3;
+func_str = s7;
 
 f = inline(func_str, 'r', 'theta');
 
@@ -43,10 +43,9 @@ MM = 2 * M;
 
 
 
-
 maxDegree = 49;
 
-numQuad = 10:10:maxDegree * 2;
+numQuad = 10:2:maxDegree * 2;
 
 errors = zeros(1, length(numQuad));
 
@@ -64,7 +63,7 @@ for ii = 1:length(numQuad)
     % The exact value
     fexact = zeros(NN, MM);
     for k = 1:NN
-        fexact(k, :) = f(r_c(k), theta_c(:));
+        fexact(k, :) = f(r_c(k), theta_c);
     end
     
     [errors(ii), a] = ZernInterpError(func_str, N, M, fexact, r_c, theta_c, maxDegree, numQuad(ii));
@@ -79,4 +78,6 @@ plot(numQuad, log10(errors), 'k', 'LineWidth', 2);
 xlabel('Number of quadrature points', 'FontSize', 14);
 ylabel('log10 of interpolation errors', 'FontSize', 14);
 %title(sprintf('Zernike Interpolation of %s', func_str), 'FontSize', 14);
-title('Zernike Interpolation of 1 / (1 + 3^{2} * r^{2})', 'FontSize', 14);
+%title('Zernike Interpolation of 1 / (1 + 3^{2} * r^{2})', 'FontSize', 14);
+%title('Zernike Interpolation of  cos(10 * r * cos(\theta))', 'FontSize', 14); %s12
+title('Zernike Interpolation of exp(-3*(r*cos(\theta))^2+(r*sin(\theta))^2)/(1+r^2)', 'FontSize', 14);%s7

55f6d8a5042102c66e6512df5897b87f5c6035d2
yufu.fisher <yufu.fisher@949dbb96-0d51-11df-8f49-cf7c468afe83>
2010-02-04 02:41:38 +0000

    File structure are cleaned

---
+++ trunk/cv/imagelab/alg/fd/EdgeDector.cpp
@@ -0,0 +1,98 @@
+#include "EdgeDector.h"
+#include "highgui.h"
+
+void EdgeDector::detectEdges(const cv::Mat & img, cv::Mat & edges) {
+	this->img = img;
+	_detectEdges(edges);
+}
+
+void EdgeDector::detectEdges(cv::Mat & edges)
+{
+	_detectEdges(edges);
+}
+
+/*void EdgeDector::detectEdges(QImage & qEdges)
+{
+	detectEdges(_edges);
+	qEdges = QImage(_edges.data, _edges.cols, _edges.rows, QImage::Format_RGB888);
+	}*/
+
+EdgeDector::EdgeDector(const char * filename)
+{
+	img = cv::imread(filename);
+}
+
+CannyDector::CannyDector() : EdgeDector(img)
+{
+	init();
+}
+
+CannyDector::CannyDector(const cv::Mat & img) : EdgeDector(img)
+{
+	init();
+}
+
+CannyDector::CannyDector(const char * filename)
+{
+	img = cv::imread(filename, 0);
+	dummy(0);
+	init();
+}
+
+CannyDector::CannyDector(const cv::Mat & img,
+						 cv::Mat & edges,
+				 	 	 double low,
+						 double high,
+						 double apertureSize,
+						 bool L2gradient) : EdgeDector(img)
+{
+	setParam(low, high, apertureSize, L2gradient);
+	//EdgeDector::detectEdges(img, edges);
+	detectEdges(img, edges);
+}
+
+void CannyDector::init()
+{
+			lowThresh = 100;
+			highThresh = 150;
+			apertureSize = 3;
+			L2gradient = false;
+}
+
+void CannyDector::_detectEdges(cv::Mat & edges)
+{
+	cv::Canny(img, edges, lowThresh, highThresh, apertureSize, L2gradient);
+}
+
+void CannyDector::setLowThresh(double low)
+{
+	lowThresh = low;
+}
+
+void CannyDector::setHighThresh(double high)
+{
+	highThresh = high;
+}
+
+void CannyDector::setApertureSize(int apertureSize)
+{
+	this->apertureSize = apertureSize;
+}
+
+void CannyDector::setL2gradient(bool L2gradient)
+{
+	this->L2gradient = L2gradient;
+}
+
+void CannyDector::setThresh(double low, double high)
+{
+	lowThresh =low;
+	highThresh = high;
+}
+
+void CannyDector::setParam(double low, double high, double apertureSize, bool L2gradient)
+{
+	setThresh(low, high);
+	this->apertureSize = apertureSize;
+	this->L2gradient = L2gradient;
+}
---
+++ trunk/cv/imagelab/alg/fd/EdgeDector.h
@@ -0,0 +1,61 @@
+#ifndef EDGEDECTOR_H
+#define EDGEDECTOR_H
+
+#include "cv.h"
+//#include <QImage>
+
+class EdgeDector {
+
+public:
+	EdgeDector() {}
+	EdgeDector(const cv::Mat & img) { this->img = img;}
+	EdgeDector(const char * filename);
+
+	void setImage(const cv::Mat & img) { this->img = img;}
+	void detectEdges(cv::Mat & edges);
+	void detectEdges(const cv::Mat & img, cv::Mat & edges);
+	//void detectEdges(QImage & qEdges);
+	void dummy(int i){}
+	
+	virtual ~EdgeDector(){}
+
+protected:
+	cv::Mat img;
+	cv::Mat _edges;
+	// The algorithm is wrapped in this virtual function
+	virtual void _detectEdges(cv::Mat & edges) = 0;
+};
+
+class CannyDector : public EdgeDector
+{
+public:
+	CannyDector();
+	CannyDector(const cv::Mat & img);
+	CannyDector(const char * filename);
+	CannyDector(const cv::Mat & img,
+				cv::Mat & edges,
+				double low,
+				double high,
+				double apertureSize,
+				bool L2gradient);
+
+	// Set the parameter of Canny edge detection
+	void setLowThresh(double low);
+	void setHighThresh(double high);
+	void setThresh(double low, double high);
+	void setApertureSize(int apertureSize);
+	void setL2gradient(bool L2gradient);
+	void setParam(double low, double high, double apertureSize, bool L2gradient);
+protected:
+	void _detectEdges(cv::Mat & edges);
+private:
+	void init();
+
+private:
+	// Parameters for Canny edge detection
+	double lowThresh; //Default 100 for canny
+	double highThresh; // Default 150 for canny
+	int apertureSize; // Default 3 for canny
+	bool L2gradient; //Default false
+};	
+#endif
